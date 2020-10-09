adafa76010974c6f0e149891d768d8087de7c9b3
Joelle Everett <everettjoelle26@gmail.com>
2020-10-09 16:12:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,12 @@ app.use(express.static('public'));
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
+    res.send('Lab 5 for ${process.env.NAME}');
   })
   .post((req, res) => {
     console.log('POST request detected');
-    res.send('<p>Hello world</p>')
+    console.log('Form data in res.body', req.body);
+    res.send('hello world')
   });
 
 app.listen(port, () => {
