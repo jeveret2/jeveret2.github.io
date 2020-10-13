d778866d5970a7aaec1b321773f5823c2f50bf05
Joelle Everett <everettjoelle26@gmail.com>
2020-10-13 17:01:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,15 +13,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
+<<<<<<< HEAD
     res.send('Lab 5 for ${process.env.NAME}');
+=======
+    res.send(`Lab 5 for ${process.env.NAME}`);
+>>>>>>> 4da4279546773b6145420cfec1f42fcb85aa7ff9
   })
   .post((req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
+<<<<<<< HEAD
     res.send('hello world')
+=======
+>>>>>>> 4da4279546773b6145420cfec1f42fcb85aa7ff9
   });
 
 app.listen(port, () => {
